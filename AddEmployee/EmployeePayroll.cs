0dba0bd5be346b478689de08aa1355a1045c0449
Luv sahu <lksahu404@gmail.com>
2022-07-22 17:36:13 +0530

    [Luv SAHU] This code for add employee with help of Stored Procedure

---
+++ AddEmployee/EmployeePayroll.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET
+{
+    public class EmployeePayroll
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }  
+        public double Salary { get; set; }
+        public DateTime StartDate { get; set; }
+        public char Gender { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Department { get; set; }
+        public string Address { get; set; }
+        public double BasicSalary { get; set; }
+        public double Deductions { get; set; }
+        public double TaxablePay { get; set; }
+        public double IncomeTax { get; set; }
+        public double NetPay { get; set; }
+
+    }
+}
---
+++ AddEmployee/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ADO.NET
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeRepository employeeRepo = new EmployeeRepository();
+            EmployeePayroll model = new EmployeePayroll();
+
+            model.Name = "Rishi";
+            model.Address = "Rajiv Chowk";
+            model.PhoneNumber = 1234565432;
+            model.Salary = 3000000;
+            model.Gender = 'M';
+            employeeRepo.AddEmployee(model);
+            employeeRepo.GetAllEmployees();
+
+            employeeRepo.GetAllEmployees();
+
+
+        }
+
+        }
+    }
