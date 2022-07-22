b6a0392e45f70e2a38257199122c7cff0e78a88c
Luv sahu <lksahu404@gmail.com>
2022-07-22 18:55:53 +0530

    [Luv SAHU] This code for Insert Two Tables with help of Stored Procedure

---
+++ InsertingIntoTwoTables/InsertTwoable.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spInsertIntoTwoTable]
+(
+@Name varchar(200),
+@Address varchar(200),
+@Gender char(1),
+@Id INT OUTPUT
+)
+AS
+insert into Employee_Payroll(Name,Address,Gender) values(@Name,@Address,@Gender);
+
+SET @Id=SCOPE_IDENTITY()
+         RETURN @Id;
