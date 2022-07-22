4fcf8247c76c921670d4fdf60e06f2fa25534352
Luv <104768173+LuvSahu@users.noreply.github.com>
2022-07-22 18:56:43 +0530

    Delete AddEmployee directory

--- AddEmployee/AddEmployee.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE PROCEDURE spAddEmployee
-@Name nvarchar(200),
-@Address nvarchar(200),
-@Gender char(1),
-@Salary float,
-@PhoneNumber bigint
-AS
-insert into employee_payroll (Name,Address,Gender,Salary,PhoneNumber) values(@Name,@Address,@Gender,@Salary,@PhoneNumber);
-
