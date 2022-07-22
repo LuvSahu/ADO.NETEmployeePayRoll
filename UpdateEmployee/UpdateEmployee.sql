48033a104734e5cfb13884c5a26cbfb7494347e1
Luv <104768173+LuvSahu@users.noreply.github.com>
2022-07-22 18:35:24 +0530

    Delete UpdateEmployee directory

--- UpdateEmployee/UpdateEmployee.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE PROCEDURE spUpdateEmployee
----ALTER PROCEDURE spUpdateEmployee
-@Name varchar(200),
-@Id int,
-@BasicSalary float
-As
-update employee_payroll set BasicSalary=@BasicSalary where Id=@Id and Name=@Name;
-
-Exec spUpdateEmployee 'Rishi',8,5000001;
