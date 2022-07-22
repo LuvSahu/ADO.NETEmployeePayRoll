aa1a1f231b9a0d45894b8e41332f0c748fc9a2e4
Luv sahu <lksahu404@gmail.com>
2022-07-22 17:13:36 +0530

    [Luv SAHU] Ability to create DB of Employee Payroll and to connect that DB with program

---
+++ CreateDB/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ADO.NET
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeRepository employeeRepo = new EmployeeRepository();
+            //EmployeePayroll model = new EmployeePayroll();
+            employeeRepo.GetAllEmployees();
+
+
+        }
+
+        }
+    }
